7756a2ff4c82917265f0e5f8627bb87268f2447f
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-06-27 14:58:30 -0300

    Add CORS options

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+server.use(
+  cors({
+    origin: '*',
+    methods: 'POST',
+  }),
+);
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
